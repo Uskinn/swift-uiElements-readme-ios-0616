64d60b03c38e54eefd7ab380c4fba0b368f426aa
Uskinn <s.nevzor@yahoo.com>
2016-07-12 18:56:20 -0400

    Done

---
+++ swift-uiElements-readme-ios-0616/swift-uiElements-readme-ios-0616/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-uiElements-readme-ios-0616
+//
+//  Created by Sergey Nevzorov on 7/12/16.
+//  Copyright © 2016 Sergey Nevzorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
